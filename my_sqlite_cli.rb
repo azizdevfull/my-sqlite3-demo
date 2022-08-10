f1cfe9b3ab5c0efac9cc0896d0fde6758611aa39
Student astrum <student@astrum.domain>
2022-08-10 17:25:33 +0500

    sqlite demo

--- my_sqlite_cli.rb
+++ my_sqlite_cli.rb
@@ -1,254 +1,140 @@
-require 'date'
-require_relative 'my_sqlite_request'
+require "readline"
+require "csv"
+require_relative "./my_sqlite_request.rb"
 
-class MySqliteCLI
-  KEYWORDS = %w[select insert update delete from join where order values set]
 
-  def initialize(database)
-    @database = database
-    start_message
-    ARGV.clear
-    run
-  end
-
-  def run
-    while true
-      print 'my_sqlite_cli>'
-      input = gets.chomp
-      break if input == 'quit'
-      input_arr = parse_input(input)
-      action(input_arr)
-    end
-  end
-
-  private
-
-  def action(input)
-    req = MySqliteRequest.new
-    until input.empty?
-      case input[0].upcase
-      when 'SELECT'
-        handle_select(req, input)
-      when 'INSERT'
-        handle_insert(req, input)
-      when 'UPDATE'
-        handle_update(req, input)
-      when 'DELETE'
-        handle_delete(req, input)
-      when 'FROM'
-        handle_from(req, input)
-      when 'JOIN'
-        handle_join(req, input)
-      when 'WHERE'
-        handle_where(req, input)
-      when 'ORDER'
-        handle_order(req, input)
-      when 'VALUES'
-        handle_values(req, input)
-      when 'SET'
-        handle_set(req, input)
-      else
-        raise 'Invalid syntax'
-      end
-    end
-    run_query(req)
-  end
-
-  def handle_from(req, input)
-    remove_extra_quotations(input[1])
-    req.from(input[1])
-    input.shift(2)
-  end
-
-  def handle_select(req, input)
-    column_names = []
-    while true
-      input.shift
-      if input.first.end_with?(',')
-        column_names << input.first[0...-1]
-      else
-        column_names << input.first
-        break
-      end
-    end
-    input.shift
-    column_names.each { |col| remove_extra_quotations(col) }
-    req.select(*column_names)
-  end
-
-  def handle_insert(req, input)
-    raise "Must be INSERT INTO" unless input[1].upcase == 'INTO'
-
-    remove_extra_quotations(input[2])
-    req.insert(input[2])
-    input.shift(3)
-  end
-
-  def handle_update(req, input)
-    remove_extra_quotations(input[1])
-    req.update(input[1])
-    input.shift(2)
-  end
-
-  def handle_delete(req, input)
-    req.delete
-    input.shift
-  end
-
-  def handle_join(req, input)
-    raise 'Invalid join syntax' if input[5].nil? || 
-                                   input[2].upcase != 'ON' || 
-                                   input[4] != '='
-            
-    table_b = input[1]
-    col_a = input[3]
-    col_b = input[5]
-    [col_a, table_b, col_b].each { |col| remove_extra_quotations(col) }
-    req.join(col_a, table_b, col_b)
-    input.shift(6)
-  end
-
-  def handle_order(req, input)
-    raise 'Invalid order syntax' if input[1].upcase != 'BY' || input[2].nil?
-
-    col_name = remove_extra_quotations(input[2])
-    shift = 3
-    order = remove_extra_quotations(input[3].upcase) if input[3]
-    if %w[ASC DESC].include?(order)
-      req.order(col_name, order)
-      shift += 1
-    else
-      req.order(col_name)
-    end
-    input.shift(shift)
-  end
-
-  def handle_values(req, input)
-    values = []
-    raise 'Invalid values syntax' unless input[1] == '('
-    input.shift
-    while true
-      input.shift
-      raise 'Invalid values syntax' unless input[0][-1] == ',' || input[0][-1] == ')'
-
-      values << remove_extra_quotations(input.first[0...-1])
-      break unless input.first.end_with?(',')
-    end
-    input.shift
-    req.values(*values)
-  end
-
-  def handle_set(req, input)
-    data = {}
-    input.shift
-    while true
-      col = input[0]
-      raise 'Invalid syntax' unless input[1] == '='
-
-      if input[2].end_with?(',')
-        val = remove_extra_quotations(input[2][0...-1])
-        data[col] = val
-        input.shift(3)
-      else
-        val = remove_extra_quotations(input[2])
-        data[col] = val
-        input.shift(3)
-        break
-      end
-    end
-    req.set(data)
-  end
-
-  def handle_where(req, input)
-    input.shift
-    where_type = input[1].upcase
-    if where_type == '='
-      single_where(req, input)
-    elsif where_type == 'IN'
-      multi_where(req, input)
-    else
-      raise 'Invalid syntax'
-    end
-  end
-
-  def single_where(req, input)
-    col_name = input[0]
-    criteria = input[2]
-    raise 'Invalid where syntax' if criteria.nil?
-
-    [col_name, criteria].each { |col| remove_extra_quotations(col) }
-    req.where(col_name, criteria)
-    input.shift(3)
-  end
-
-  def multi_where(req, input)
-    criterias = []
-    col_name = input.first
-    raise 'Invalid where syntax' if input[2] != '('
-    input.shift(3)
-
-    while true
-      raise 'Invalid values syntax' unless input[0][-1] == ',' || input[0][-1] == ')'
-
-      criterias << remove_extra_quotations(input.first[0...-1])
-      break unless input.first.end_with?(',')
+class MySqliteQueryCli
     
-      input.shift
-    end
-    input.shift
-    remove_extra_quotations(col_name)
-    req.where(col_name, criterias)
-  end
 
-  def run_query(req)
-    req.query_type == :select ? output_query_results(req.run) : req.run
-  end
+    def my_parser(str)
+       str = str.split
 
-  def output_query_results(results)
-    results.each do |result| 
-      result.values.each_with_index do |val, i|
-        print val
-        if i == (result.length - 1)
-          print "\n"
-        else
-          print '|'
+       str.each {|element|
+       if element.include?(",")
+        str[str.index(element)] = element.split(",")
+       end
+       if element.include?("=")
+        str[str.index(element)] = element.split("=")
+       end
+    }
+    str.flatten!
+    0.upto(str.length-1)do |i|
+    if str[i] != nil
+        if str[i][0]=="("
+            str[i].delete_prefix!("(")
+            str.last.delete_suffix!(")")
         end
-      end
+        if str[i][0]=="'" && str[i][str[i].length - 1] != "'"
+            i += 1
+            loop do
+                    if str[i][str[i].length - 1] !="'"
+                        str[i-1]<<""<<str[i]
+                        str.delete_at(i)
+                    else
+                        str[i-1]<<""<< str[i]
+                        str.delete_at(i)
+                        break
+                    end
+                end
+            end
+
+        end
     end
-  end
-
-  def parse_input(input)
-    final = input[-1]
-    raise 'Query must end with semiclolon!' unless final == ';'
-    line = add_space_to_left_paren(input[0...-1])
-    CSV.parse_line(line, col_sep: ' ', quote_char: "'", liberal_parsing: true)
-  end
-
-  # Only does this if it's not inside of quotes
-  def add_space_to_left_paren(line)
-    stack = []
-    line.each_char.with_index do |c, i|
-      if c == "'"
-        stack.empty? ? stack << c : stack.pop
-      elsif c == '(' && stack.empty?
-          line[i] = '( '
-          break
-      end
+    str
     end
-    line
-  end
 
-  def remove_extra_quotations(col)
-    col[0] = '' if col[0] == "'" || col[0] == '"'
-    col[-1] = '' if col[-1] == "'" || col[-1] == '"'
-    col
-  end
 
-  def start_message
-    puts "MySQLite version 0.1 #{Date.today}"
-  end
+    def run!
+
+        while buf = Readline.readline("my_sqlite_cli>>",true)
+            request    = MySqliteRequest.new
+            key_word   = ""
+            select_columns       = []
+            where_params_ar      = []
+            update_attributes_ar = []
+            values_ar  = []
+            my_parser(buf).each {|word|
+            if word[0]=="'" && word[word.length-1]=="'"
+                word.delete_prefix!("'").delete_suffix!("'")
+            end
+
+            case word.upcase
+            when "SELECT"
+                key_word = word
+                request.instance_variable_set(:@type_of_request, :select)
+                next
+            when "INSERT"
+                key_word = word
+                request.instance_variable_set(:@type_of_request, :insert)
+                next
+            when "INTO"
+                key_word = word
+                next
+            when "UPDATE"
+                key_word = word
+                request.instance_variable_set(:@type_of_request, :update)
+                next
+            when "DELETE"
+                key_word = word
+                request.instance_variable_set(:@type_of_request, :delete)
+                next
+            when "FROM"
+                key_word = word
+                next        
+            when "WHERE"
+                key_word = word
+                next  
+            when "SET"
+                key_word = word
+                next        
+            when "VALUES"
+                key_word = word
+                next  
+            end
+
+            if key_word == ""
+                p "Input can't be blank! Use 'SELECT', 'INSERT', 'UPDATE', 'DELETE' for query"
+                exit
+            end
+
+            case key_word.upcase
+            when "SELECT"
+                select_columns<<word
+                request.instance_variable_set(:@select_columns, select_columns)
+                next
+            when "INTO"
+                request.instance_variable_set(:@table_name, word)
+                next
+            when "UPDATE"
+                request.instance_variable_set(:@table_name, word)
+                next
+            when "FROM"
+                request.instance_variable_set(:@table_name, word)
+                headers = CSV.open(word,&:readline)
+                next
+            when "WHERE"
+                where_params_ar<<word
+                next
+            when "SET"
+                update_attributes_ar<<word
+                next
+            when "VALUES"
+                values_ar<<word
+                next
+            end           
+        }
+        headers = CSV.open(request.instance_variable_get(:@table_name), &:readline)
+        insert_attributes_hash = Hash[headers.zip(values_ar)].delete_if{|key,value| value ==nil}
+        request.instance_variable_set(:@insert_attributes, insert_attributes_hash)
+        request.instance_variable_set(:@where_params, where_params_ar.each_slice(2).to_a)
+
+        request.instance_variable_set(:@update_attributes, update_attributes_ar.each_slice(2).to_h)
+
+        request.run   
+
+        end
+    end
 end
 
-if __FILE__ == $PROGRAM_NAME
-  MySqliteCLI.new(ARGV[0])
-end
+msqcli = MySqliteQueryCli.new
+msqcli.run!
--- my_sqlite_request.rb
+++ my_sqlite_request.rb
@@ -1,274 +1,277 @@
 require 'csv'
+require 'json'
+=begin
+Part 1 Describing scope of project
+
+# SELECT QUERY
+# INSERT QUERY
+# UPDATE
+# DELETE
+
+# 1.Type of request
+# 2. Set settings
+# 3. Run
+
+
+=end
 class MySqliteRequest
-  ORDER = %w[DESC ASC].freeze
 
-  attr_reader :query_type
+    def initialize
+        @type_of_request = :none
+        @select_columns  = []
+        @where_params    = []
+        @insert_attributes = {}
+        @update_attributes = {}
+        @table_name      = nil
+        #@order           = :asc
 
-  def initialize(table_name = nil)
-    @table_name = table_name
-    @table_name << '.csv' unless @table_name.nil? || @table_name.end_with?('.csv')
-    @query_type = nil
-    @joins = []
-    @wheres = []
-    @select = []
-    @order = []
-    @values = nil
-    @set = nil
-    @table = nil
-    @columns = nil
-    @id = nil
-  end
-
-  def run
-    run_errors
-    @table, @columns = form_table(@table_name)
-    check_column_mismatch
-    join_tables
-    case @query_type
-    when :select
-      run_select
-    when :insert
-      run_insert
-    when :update
-      run_update
-    when :delete
-      run_delete
     end
-  end
 
-  def from(table_name)
-    raise "Can't have two FROMs" if @table_name
-
-    @table_name = table_name
-    @table_name << '.csv' unless @table_name.nil? || @table_name.end_with?('.csv')
-    self
-  end
-
-  def select(*column_names)
-    @query_type ||= :select
-    one_query_type(:select)
-
-    @select.concat(column_names.map(&:to_sym))
-    self
-  end
-
-  # Criteria can be a string or an array of strings
-  def where(column_name, criteria)
-    @wheres << [column_name.to_sym, criteria]
-    self
-  end
-
-  def join(col_a, table_name_b, col_b)
-    table_name_b << '.csv' unless table_name_b.nil? || table_name_b.end_with?('.csv')
-    @joins << [col_a.to_sym, table_name_b, col_b.to_sym]
-    self
-  end
-
-  def order(column_name, order = nil)
-    order = 'ASC' if order.nil?
-    raise 'Order must be ASC or DESC' unless ORDER.include?(order.upcase)
-
-    @order << [order.upcase.to_sym, column_name.to_sym]
-    self
-  end
-
-  def insert(table_name)
-    raise "Can't have two FROMs" if @table_name
-    @table_name = table_name
-    @table_name << '.csv' unless @table_name.nil? || @table_name.end_with?('.csv') 
-    @query_type ||= :insert
-    one_query_type(:insert)
-    self
-  end
-
-  def values(*data)
-    raise "Can't have multiple values" if @values
-    raise "Can't have value and set" if @set
-
-    @values = data
-    self
-  end
-
-  def update(table_name)
-    raise "Can't have two FROMs" if @table_name
-
-    @table_name = table_name
-    @table_name << '.csv' unless @table_name.nil? || @table_name.end_with?('.csv') 
-    @query_type ||= :update
-    one_query_type(:update)
-    self
-  end
-
-  def set(data)
-    raise "Can't have multiple sets" if @set
-    raise "Can't have value and set" if @values
-
-    @set = data.transform_keys(&:to_sym)
-    self
-  end
-
-  def delete
-    @query_type ||= :delete
-    one_query_type(:delete)
-    self
-  end
-
-  private
-
-  def one_query_type(query_type)
-    raise "Can't have different query types" unless @query_type == query_type
-  end
-
-  def run_errors
-    raise 'Must have a table!' unless @table_name
-    raise 'Must have a query type!' unless @query_type
-    raise 'Order and join can only be used with select!' if @query_type != :select &&
-                                                            (@order.any? || @joins.any?)
-    raise 'Insert must have values!' if @query_type == :insert && !@values
-    raise 'Update must have set!' if @query_type == :update && !@set
-    raise "Insert can't have where!" if @query_type == :insert && @wheres.any?
-    raise "Select can't have values or set" if @query_type == :select && (@values || @set)
-  end
-
-  def check_column_mismatch
-    raise "Insert column value mismatch!" if @query_type == :insert && @columns.size != (@values.size + 1)
-  end
-
-  def form_table(table_name)
-    table = {}
-    columns = nil
-    File.foreach(table_name).with_index do |line, idx|
-      if idx.zero?
-        columns = line.chomp.split(',').map(&:to_sym) 
-        columns << :id
-      else
-        row = {}
-        CSV.parse_line(line).each_with_index { |val, i| row[columns[i]] = val }
-        row[:id] = idx
-        table[idx] = row
-      end
+    def from(table_name)
+        @table_name = table_name
+        self
     end
-    @id = table.size + 1
-    [table, columns]
-  end
 
-  def join_tables
-    @joins.each do |join|
-      col_a = join[0]
-      table_b, columns_b = form_table(join[1])
-      col_b = join[2]
-
-      join_table(col_a, col_b, table_b, columns_b)
-    end
-  end
-
-  def join_table(col_a, col_b, table_b, columns_b)
-    @columns = (@columns + columns_b).uniq
-    new_table = {}
-    i = 1
-    @table.each do |row_id, _v|
-      table_b.each do |rowb_id, _v|
-        if @table[row_id][col_a] == table_b[rowb_id][col_b]
-          new_table[i] = @table[row_id].merge(table_b[rowb_id])
-          new_table[i][:id] = i
-          i += 1
+    def select(columns)
+        if(columns.is_a?(Array))
+            @select_columns += columns.collect{ |elem| elem.to_s}
+        else
+            @select_columns << columns.to_s 
         end
-      end
+
+        self._SetTypeOfRequest(:select)
+        self
     end
-    @table = new_table
-  end
 
-  def run_select
-    filtered = select_where
-    if @select.include?(:*)
-      selected = filtered.map { |_id, row| row }
-    else
-      selected = filtered.map { |_id, row| row.select { |k, _v| @select.include?(k) } }
+    def where(column_name, criteria)
+        @where_params << [column_name, criteria]
+        self
     end
-    order_table(selected)
-  end
 
-  def select_where
-    return @table if @wheres.empty?
+    #def join(column_on_db_a, filename_db_b, column_on_db_b)
+    #   self
+    #end
 
-    @table.select do |_, row|
-      check_where(row)
+    #def order(order, column_name)
+    #   self
+    #end
+
+    def insert(table_name)
+        self._SetTypeOfRequest(:insert)
+        @table_name = table_name
+        self
     end
-  end
 
-  def order_table(table)
-    return table if @order.empty?
-
-    table.sort do |a, b|
-      res = 0
-      @order.each do |order, column|
-        res = sort_column(a[column], b[column], order)
-        break unless res.zero?
-      end
-      res
+    def values(data)
+        if (@type_of_request == :insert)
+            @insert_attributes = data
+        else
+            raise "Wrong type of request to call values ()"
+        end
+        self
     end
-  end
 
-  def sort_column(a, b, order)
-    if order == :ASC
-      a <=> b
-    else
-      b <=> a
+    def update(table_name)
+        self._SetTypeOfRequest(:update)
+        @table_name = table_name
+        self
     end
-  end
 
-  def run_insert
-    new_row = {}
-    @columns[0...-1].each.with_index { |col, i| new_row[col] = @values[i] }
-    @table[@id] = new_row
-    @id += 1
-    change_db
-  end
+    #def set(data)
+    #    self
+    #end
 
-  def run_delete
-    if @wheres.empty?
-      @table = {}
-    else
-      delete_rows
+    def delete
+        self._SetTypeOfRequest(:delete)
+        self
     end
-    change_db
-  end
 
-  def delete_rows
-    @table.each do |id, row|
-      @table.delete(id) if check_where(row)
+    def print_select_type
+        puts "Select Attributes #{@select_columns}"
+        puts "Where Attributes #{@where_params}"
     end
-  end
 
-  def run_update
-    if @wheres.empty?
-      @table.each { |_id, row| update_row(row) }
-    else
-      @table.select do |_, row|
-        update_row(row) if check_where(row)
-      end
+    def print_insert_type
+        puts "Insert Attributes #{@insert_attributes}"
     end
-    change_db
-  end
 
-  def check_where(row)
-    @wheres.all? do |where| 
-      if where[1].is_a? Array
-        where[1].include?(row[where[0]])
-      else
-        row[where[0]] == where[1] 
-      end
+    def print
+        puts "Type Of Request #{@type_of_request.upcase}"
+        puts "Table Name #{@table_name}"
+        if (@type_of_request == :select)
+            print_select_type
+        elsif 
+            (@type_of_request == :insert)
+            print_insert_type
+        end
     end
-  end
 
-  def update_row(row)
-    @set.each { |k, v| row[k] = v }
-  end
-
-  def change_db
-    CSV.open(@table_name, 'w') do |csv|
-      csv << @columns[0...-1]
-      @table.each { |_id, row| csv << row.reject { |k, _v| k == :id }.values }
+    def run
+        print
+        if (@type_of_request == :select)
+            _run_select
+        elsif 
+            (@type_of_request == :insert)
+            _run_insert
+        elsif 
+            (@type_of_request == :update)
+            _run_update
+        elsif 
+            (@type_of_request == :delete)
+            _run_delete        
+        end
     end
-  end
+
+    def _SetTypeOfRequest(new_type)
+        if (@type_of_request == :none or @type_of_request == new_type)
+            @type_of_request = new_type
+        else
+            raise "Invalid: type of request already set to #{@type_of_request} (new type => #{new_type}) "
+        end
+    end
+
+    
+
+    def _run_select
+        result = []
+        CSV.parse(File.read(@table_name), headers: true).each do |row|
+            if @select_columns == ["*"]
+                @select_columns=CSV.open(*@table_name,&:readline)
+            end
+
+            if @where_params==[]
+                result << row.to_hash.slice(*@select_columns).values
+            else   
+            @where_params.each do |where_attribute|
+                if row [where_attribute[0]] == where_attribute[1]
+                    result << row.to_hash.slice(*@select_columns).values
+                end
+            end
+        end
+    end
+        puts @select_columns.to_csv
+        result.each {|el|
+        puts el.to_csv
+        }
+                
+    end
+
+    def _run_insert
+        if @where_params ==[]
+
+        File.open(@table_name, 'a') do|f|
+           f.puts @insert_attributes.values.join(',')
+        end
+
+        else
+            column_name = @where_params[0][0]
+            criteria = @where_params[0][1]
+            table = CSV.table(@table_name)
+            table.each do |row|
+                if row[:"#{column_name}"]== criteria
+                    @insert_attributes.each_pair {|key, value|
+                    row[:"#{key}"] = value
+                }
+            end
+        end
+        File.open(@table_name, 'w') do |f|
+            f.write(table.to_csv)
+            end
+        end
+    
+    end
+
+    def _run_update
+        column_name = @where_params[0][0]
+        criteria = @where_params[0][1]
+        table = CSV.table(@table_name)
+        table.each do |row|
+            if row[:"#{column_name}"]==criteria
+                @update_attributes.each_pair {|key,value|
+                row[:"#{key}"]=value
+            }
+            end
+        end
+        File.open(@table_name, 'w') do |f|
+            f.write(table.to_csv)
+        end
+    end
+
+    def _run_delete
+        column_name = @where_params[0][0]
+        criteria = @where_params[0][1]
+        table = CSV.table(@table_name)
+        table.delete_if do|row|
+            row[:"#{column_name}"] == criteria
+        end
+        File.open(@table_name,'w') do |f|
+            f.write(table.to_csv)
+        end
+    end
+
+
 end
+
+ def _main()
+=begin
+Questiom 0 of 10
+
+request = MySqliteRequest.new
+request = request.from('nba_player_data.csv')
+request = request.select('name')
+request.run
+
+Questions 1 of 10
+
+request = MySqliteRequest.new
+request = request.from('nba_player_data.csv')
+request = request.select('name')
+request = request.where('college', 'University of California')
+request.run
+
+
+Question 2 of 10
+
+request = MySqliteRequest.new
+request = request.from('nba_player_data.csv')
+request = request.select('name')
+request = request.where('college', 'University of California')
+request = request.where('year_start', '1997')
+request.run
+
+Question 3 of 10
+
+    request = MySqliteRequest.new
+    request = request.insert('nba_player_data_light.csv')
+    request = request.values({"name"=>"Don Adams","year_start"=>"1971","year_end"=>"1977",
+    "position"=>"F","height"=>"6-6","weight"=>"210","birth_date"=>"November 27, 1947",
+    "college"=>"Northwestern University"})
+    request.run
+
+Question 4 of 10
+
+request = MySqliteRequest.new
+request = request.insert('nba_player_data.csv')
+request = request.values('name' => 'Alaa Renamed')
+request = request.where('name', 'Alaa Abdenlaby')
+request.run
+
+Question 5 of 10
+
+request = MySqliteRequest.new
+request = request.delete()
+request = request.from('nba_player_data_light.csv')
+request = request.where('name', 'Don Adams')
+request.run
+
+=end    
+
+
+    
+ end
+ _main
+
+
+ 
+
+ 
